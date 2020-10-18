c0c2d3da84003a2d748154719304267b42e28af7
E-Rain <283866748@qq.com>
2020-10-18 23:36:59 +0800

    no.1-flat.js

---
+++ flat.js
@@ -0,0 +1,20 @@
+const flat = (array, num) => {
+	let arr = [];
+	function _flat(args, n) {
+		if (Array.isArray(args) && n === 0) {
+			return args;
+		}
+		n--;
+		args.flatMap(v => {
+			if (Array.isArray(v)) {
+				if (Array.isArray(_flat(v, n))) {
+					arr.push(v);
+				}
+			} else {
+				arr.push(v);
+			}
+		});
+	}
+	_flat(array, num + 1);
+	return arr;
+};
